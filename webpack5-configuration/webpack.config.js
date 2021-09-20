43eb33e7f3b3a7fe6520d856f2e390a119009ff8
David Barcenas <daveepro@outlook.com>
2021-09-20 12:57:30 -0500

    start react app and install babel

--- webpack5-configuration/webpack.config.js
+++ webpack5-configuration/webpack.config.js
@@ -3,5 +3,16 @@ const path = require('path')
 module.exports = {
   output: {
     path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react']
+        }
+      }
+    ]
   }
 }
