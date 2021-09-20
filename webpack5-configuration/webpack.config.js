98f4acc4a87f0c39a682f4371071b8c8423b4a19
David Barcenas <daveepro@outlook.com>
2021-09-20 16:56:12 -0500

    add style-loader and css-loader

--- webpack5-configuration/webpack.config.js
+++ webpack5-configuration/webpack.config.js
@@ -1,18 +1,33 @@
 const path = require('path') 
 
+const ruleForStyles = {
+  test: /\.css$/,
+  use: ['style-loader', 'css-loader']
+}
+
+const ruleForJavaScript = {
+  test: /\.js$/,
+  loader: 'babel-loader',
+  options: {
+    presets: [
+      [
+        '@babel/preset-react',
+        {
+          runtime: 'automatic'
+        }
+      ]
+    ]
+  }
+}
+
+const rules = [
+  ruleForJavaScript,
+  ruleForStyles
+]
+
 module.exports = {
   output: {
     path: path.resolve(__dirname, 'build')
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-react']
-        }
-      }
-    ]
-  }
+  module: { rules }
 }
