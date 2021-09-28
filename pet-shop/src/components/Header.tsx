06bd43c2bd740ea7d945d86dc578a8c1468d6300
David Barcenas <daveepro@outlook.com>
2021-09-27 20:08:59 -0500

    create header and banner layout

--- pet-shop/src/components/Header.tsx
+++ pet-shop/src/components/Header.tsx
@@ -1,21 +1,34 @@
 import { Link } from "react-router-dom"
+import '../styles/components/header.scss'
 
 export const Header = () => {
   return (
     <header className="Header">
-      <h1 className="Header-title">
-        <Link to="/">Gaming Store</Link>
-      </h1>
-      <div className="Header-checkout">
-        <Link to="/checkout">
-          Checkout
-        </Link>
+      <div className="Header-bar">
+        <div className="container d-flex-between">
+        <span>+(52) 89-58-97-23</span>
+        <span>30% discount on your first order</span>
+        <span>Account</span>
+        </div>
       </div>
-      <span className="material-icons">home</span>
-      <span className="material-icons-outlined">search</span>
-      <span className="material-icons-round">
-home
-</span>
+
+      <div className="container d-flex-between">
+        <h1 className="Header-title">
+          <Link to="/">Gaming Store</Link>
+        </h1>
+
+        <nav className="Header-nav">
+          <Link to="/">Home</Link>
+          <Link to="/">Catalog</Link>
+          <Link to="/">Contact</Link>
+        </nav>
+
+        <div className="Header-checkout">
+          <Link to="/checkout">
+            <span className="material-icons"> shopping_cart </span>
+          </Link>
+        </div>
+      </div>
     </header>
   )
 }
--- pet-shop/src/containers/Home.tsx
+++ pet-shop/src/containers/Home.tsx
@@ -1,8 +1,29 @@
 import { Products } from "../components/Products"
 import initialState from "../initialState"
 
+import '../styles/containers/home.scss'
+
 export const Home = () => {
   return (
-    <Products products={initialState.products} />
+    <>
+      <div className="Home-banner">
+        <div className="Home-banner-title">
+          <p>Create your own</p>
+          <p>gaming desktop</p>
+        </div>
+        <figure className="Home-banner-img">
+          <img src="https://images.pexels.com/photos/7238759/pexels-photo-7238759.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Gaming Desktop" />
+          <figcaption>
+            <p>It is high time that you have a comfortable</p>
+            <p>desk to play, work and carry out</p>
+            <p>all your day-to-day tasks.</p>
+            <button className="btn">Shop Now 
+              <span className="material-icons"> east </span>
+            </button>
+          </figcaption>
+        </figure>
+      </div>
+      <Products products={initialState.products} />
+    </>
   )
 }
