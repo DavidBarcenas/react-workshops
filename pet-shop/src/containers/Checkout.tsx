5efdb108a7fb409eff0e32bc71d2006952e9e328
David Barcenas <daveepro@outlook.com>
2021-09-28 18:44:10 -0500

    add context to checkout page

--- pet-shop/src/containers/Checkout.tsx
+++ pet-shop/src/containers/Checkout.tsx
@@ -1,7 +1,23 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/containers/checkout.scss'
+
+import { Storecontext } from '../context/StoreContext';
+import type { Product } from '../types/product';
+import '../styles/containers/checkout.scss';
 
 export const Checkout = () => {
+  const { state, removeFromCart } = useContext(Storecontext);
+  const { cart } = state;
+
+  const handleSumTotal = (): number => {
+    const sum = cart.reduce(reducer, 0);
+    return sum;
+  };
+
+  const reducer = (acc: number, curr: Product): number => {
+    return acc + curr.price;
+  };
+
   return (
     <div className="Checkout">
       <div className="container">
@@ -13,46 +29,53 @@ export const Checkout = () => {
 
         <div className="d-flex-between Checkout-wrapper">
           <div className="Checkout-content">
-            <div className="d-flex-between Checkout-item">
-              <div className="Checkout-img">
-                <img src='https://image.api.playstation.com/vulcan/img/rnd/202106/0915/yhB6VOtVuKMcvzI25vsaCmIy.png' alt="Battlefield" />
-                <div>
-                  <h3>Battlefield 2042</h3>
-                  <span>$120</span>
+            {cart.map(p => (
+              <div className="d-flex-between Checkout-item">
+                <div className="Checkout-img">
+                  <img src={p.image} alt={p.title} />
+                  <div>
+                    <h3>{p.title}</h3>
+                    <span>${p.price}</span>
+                  </div>
+                </div>
+
+                <div className="Checkout-quantity">
+                  <span>Quantity</span>
+                  <input type="text" />
+                </div>
+
+                <div className="Checkout-price">
+                  <span className="price">${p.price}</span>
+                  <button type="button" onClick={() => removeFromCart(p.id)}>
+                    <span className="material-icons"> delete_outline </span>
+                  </button>
                 </div>
               </div>
-                
-              <div className="Checkout-quantity">
-                <span>Quantity</span>
-                <input type="text" />
-              </div>
-
-              <div className="Checkout-price">
-                <span className="price">$120</span>
-                <button type="button">
-                  <span className="material-icons"> delete_outline </span>
-                </button>
-              </div>
-            </div>
+            ))}
           </div>
 
           <div className="Checkout-sidebar">
             <div className="Checkout-orders">
               <h3>Order list</h3>
-              <p>Total 1 item(s)</p>
-              <p> Total price <span>$38.00</span></p>
-              <Link to="/checkout/detail">
-                <button type="button" className="btn">Proceed to checkout</button>
-              </Link>
+              <p>Total {cart.length} item(s)</p>
+              <p>
+                Total price <span>${handleSumTotal()}</span>
+              </p>
+              {cart.length > 0 && (
+                <button type="button" className="btn">
+                  <Link to="/checkout/detail" aria-disabled>
+                    Proceed to checkout
+                  </Link>
+                </button>
+              )}
             </div>
 
             <div className="Checkout-note">
               <p> At the moment we only process orders in dollars. </p>
             </div>
           </div>
-
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
