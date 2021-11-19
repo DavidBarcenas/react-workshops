cc0ca8da2287183cf168ba3f2d16387f55de8370
David Barcenas <daveepro@outlook.com>
2021-11-19 15:40:01 -0600

    type product card

--- patterns/src/compound-component/components/ProductCard.tsx
+++ patterns/src/compound-component/components/ProductCard.tsx
@@ -1,15 +1,21 @@
 import useCounter from '../hooks/useCounter';
 import styles from '../styles/styles.module.css';
-// import noImage from '../assets/no-image.jpg';
+import noImage from '../assets/no-image.jpg';
+import type { Product } from '../../types/product';
 
-function ProductCard(): JSX.Element {
+type Props = {
+  product: Product;
+};
+
+function ProductCard({ product }: Props): JSX.Element {
   const value = 1;
   const { counter, increaseBy } = useCounter(value);
+  const image = product.image || noImage;
 
   return (
     <div className={styles.productCard}>
-      <img src="./coffee-mug.png" alt="Coffee Mug" className={styles.productImg} />
-      <span className={styles.productDescription}>Coffe Mug</span>
+      <img src={image} alt={product.title} className={styles.productImg} />
+      <span className={styles.productDescription}>{product.title}</span>
 
       <div className={styles.buttonsContainer}>
         <button className={styles.buttonMinus} onClick={() => increaseBy(-value)}>
--- patterns/src/compound-component/pages/Shopping.tsx
+++ patterns/src/compound-component/pages/Shopping.tsx
@@ -1,11 +1,23 @@
 import ProductCard from '../components/ProductCard';
+import styles from '../styles/styles.module.css';
+import type { Product } from '../../types/product';
+
+const products: Product[] = [
+  { id: '1', title: ' Coffee Mug', image: './coffee-mug.png' },
+  { id: '2', title: ' Other' },
+];
 
 function ShoppingPage(): JSX.Element {
   return (
     <div>
       <h1>Shopping Page</h1>
       <hr />
-      <ProductCard />
+
+      <div className={styles.products}>
+        {products.map(p => (
+          <ProductCard key={p.id} product={p} />
+        ))}
+      </div>
     </div>
   );
 }
