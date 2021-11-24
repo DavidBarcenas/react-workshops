0ad794034e67231bdf6ac37e9f4bc30129fdf0fe
David Barcenas <daveepro@outlook.com>
2021-11-23 19:30:25 -0600

    control counter

--- patterns/src/compound-component/hooks/useCounter.ts
+++ patterns/src/compound-component/hooks/useCounter.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { Product, ProductChangeArgs } from "../../types/product";
 
 type Counter = {
@@ -16,12 +16,17 @@ const DEFAULT_VALUE = 0
 
 function useCounter({onChange, product, value = DEFAULT_VALUE}: Props): Counter {
   const [counter, setCounter] = useState<number>(value);
+  const isControlled = useRef<boolean>(!!onChange)
 
   useEffect(() => {
     setCounter(value);
   }, [value])
   
   function increaseBy(value: number) {
+    if(isControlled.current && onChange) {
+      return onChange({count: value, product})
+    }
+
     const newValue = Math.max(counter + value, DEFAULT_VALUE)
     setCounter(newValue);
     onChange && onChange({count: newValue, product})
