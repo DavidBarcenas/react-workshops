66b2f136105ba8826ff66e383f512e3bc1b3def0
David Barcenas <daveepro@outlook.com>
2021-11-27 21:17:37 -0600

    use getFieldProps method to simplify input params

--- custom-forms/src/pages/FormikBasic.tsx
+++ custom-forms/src/pages/FormikBasic.tsx
@@ -4,7 +4,7 @@ import * as Yup from 'yup';
 const MAX_LENGTH = 15;
 
 function FormkBasicPage(): JSX.Element {
-  const { values, errors, touched, handleChange, handleSubmit, handleBlur } = useFormik({
+  const { errors, touched, handleSubmit, getFieldProps } = useFormik({
     initialValues: {
       name: '',
       lastname: '',
@@ -25,25 +25,20 @@ function FormkBasicPage(): JSX.Element {
   return (
     <>
       <h1 className="section-title">Green Form - Formik Basic</h1>
-      <form
-        autoComplete="off"
-        className="green-form"
-        onChange={handleChange}
-        onSubmit={handleSubmit}
-      >
+      <form onSubmit={handleSubmit} autoComplete="off" className="green-form">
         <div className="form-group">
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" defaultValue={values.name} onBlur={handleBlur} />
+          <input type="text" {...getFieldProps('name')} />
           {touched.name && errors.name && <span className="error">{errors.name}</span>}
         </div>
         <div className="form-group">
           <label htmlFor="lastname">Lastname</label>
-          <input type="text" name="lastname" defaultValue={values.lastname} onBlur={handleBlur} />
+          <input type="text" {...getFieldProps('lastname')} />
           {touched.lastname && errors.lastname && <span className="error">{errors.lastname}</span>}
         </div>
         <div className="form-group">
           <label htmlFor="name">Email</label>
-          <input type="email" name="email" defaultValue={values.email} onBlur={handleBlur} />
+          <input type="email" {...getFieldProps('email')} />
           {touched.email && errors.email && <span className="error">{errors.email}</span>}
         </div>
         <div className="greenFormCta">
