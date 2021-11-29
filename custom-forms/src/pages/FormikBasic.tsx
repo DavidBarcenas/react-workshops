ac1151e2a9d6ed910e1be2ad579f99ae3c215028
David Barcenas <daveepro@outlook.com>
2021-11-28 19:56:53 -0600

    use formik components

--- custom-forms/src/pages/FormikBasic.tsx
+++ custom-forms/src/pages/FormikBasic.tsx
@@ -1,51 +1,53 @@
-import { useFormik } from 'formik';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 import * as Yup from 'yup';
 
 const MAX_LENGTH = 15;
 
 function FormkBasicPage(): JSX.Element {
-  const { errors, touched, handleSubmit, getFieldProps } = useFormik({
-    initialValues: {
-      name: '',
-      lastname: '',
-      email: '',
-    },
-    validationSchema: Yup.object({
-      name: Yup.string()
-        .max(MAX_LENGTH, 'Must be 15 characters or less')
-        .required('This field is required'),
-      lastname: Yup.string()
-        .max(MAX_LENGTH, 'Must be 15 characters or less')
-        .required('This field is required'),
-      email: Yup.string().required('This field is required').email('Enter a valid email'),
-    }),
-    onSubmit: formValue => console.log(formValue),
-  });
-
   return (
     <>
       <h1 className="section-title">Green Form - Formik Basic</h1>
-      <form onSubmit={handleSubmit} autoComplete="off" className="green-form">
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input type="text" {...getFieldProps('name')} />
-          {touched.name && errors.name && <span className="error">{errors.name}</span>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="lastname">Lastname</label>
-          <input type="text" {...getFieldProps('lastname')} />
-          {touched.lastname && errors.lastname && <span className="error">{errors.lastname}</span>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="name">Email</label>
-          <input type="email" {...getFieldProps('email')} />
-          {touched.email && errors.email && <span className="error">{errors.email}</span>}
-        </div>
-        <div className="greenFormCta">
-          <button type="reset">Reset</button>
-          <button type="submit">Create</button>
-        </div>
-      </form>
+
+      <Formik
+        initialValues={{ name: '', lastname: '', email: '' }}
+        onSubmit={values => console.log(values)}
+        validationSchema={Yup.object({
+          name: Yup.string()
+            .max(MAX_LENGTH, 'Must be 15 characters or less')
+            .required('This field is required'),
+          lastname: Yup.string()
+            .max(MAX_LENGTH, 'Must be 15 characters or less')
+            .required('This field is required'),
+          email: Yup.string().required('This field is required').email('Enter a valid email'),
+        })}
+      >
+        {() => (
+          <Form autoComplete="off" className="green-form">
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <Field type="text" name="name" />
+              <ErrorMessage name="name" render={msg => <span className="error">{msg}</span>} />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="lastname">Lastname</label>
+              <Field type="text" name="lastname" />
+              <ErrorMessage name="lastname" render={msg => <span className="error">{msg}</span>} />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <Field type="email" name="email" />
+              <ErrorMessage name="email" render={msg => <span className="error">{msg}</span>} />
+            </div>
+
+            <div className="greenFormCta">
+              <button type="reset">Reset</button>
+              <button type="submit">Create</button>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 }
