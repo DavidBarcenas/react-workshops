3088cc34b6b6d43de47357ae33690cb821593060
David Barcenas <daveepro@outlook.com>
2021-11-29 15:52:40 -0600

    add dynamic validations

--- custom-forms/src/pages/DynamicForm.tsx
+++ custom-forms/src/pages/DynamicForm.tsx
@@ -1,18 +1,38 @@
 import { Form, Formik } from 'formik';
+import * as Yup from 'yup';
 import TextField from '../components/TextField';
 import formJson from '../data/custom-form.json';
 import SelectField from '../components/SelectField';
 
 const initialValues: { [key: string]: any } = {};
+const requiredFields: { [key: string]: any } = {};
 
 for (const input of formJson) {
   initialValues[input.name] = input.value || '';
+
+  if (!input.validations) continue;
+
+  let schema = Yup.string();
+
+  for (const rule of input.validations) {
+    if (rule.type === 'required') {
+      schema = schema.required();
+    }
+  }
+
+  requiredFields[input.name] = schema;
 }
+
+const validationSchema = Yup.object({ ...requiredFields });
 
 function DynamicFormPage(): JSX.Element {
   return (
     <>
-      <Formik initialValues={initialValues} onSubmit={formValues => console.log(formValues)}>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={formValues => console.log(formValues)}
+      >
         {() => (
           <Form>
             {formJson.map(({ type, name, placeholder, label, options }) => {
