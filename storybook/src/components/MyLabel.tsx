f82025bd1a990021b6253d8b99cd797b9569eb4d
David Barcenas <daveepro@outlook.com>
2021-12-01 18:16:44 -0600

    add descriptions to properties

--- storybook/src/components/MyLabel.tsx
+++ storybook/src/components/MyLabel.tsx
@@ -1,12 +1,18 @@
 import './my-label.css'
 
 interface MyLabelProps {
+  /**
+   * What text is displayed
+   */
   label: string;
+  /**
+   * What is the size of the label?
+   */
   size?: 'normal' | 'h1' | 'h2' | 'h3'
 }
 
 export const MyLabel = ({label, size = 'normal'}: MyLabelProps) => {
   return (
-    <label className={size} >{label}</label>
+    <label className={size}>{label}</label>
   )
 }
