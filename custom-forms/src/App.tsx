e5ee9f1f206d63e31917d1b306ad1749bba89449
David Barcenas <daveepro@outlook.com>
2021-12-03 18:50:09 -0600

    install ruter dom v6

--- custom-forms/src/App.tsx
+++ custom-forms/src/App.tsx
@@ -1,10 +1,18 @@
-import DynamicFormPage from './pages/DynamicForm';
+import { Outlet } from 'react-router';
+import Router from './routes/Router';
 
 function App(): JSX.Element {
   return (
-    <div className="container">
-      <DynamicFormPage />
-    </div>
+    <>
+      <aside>
+        <h1>Aside</h1>
+      </aside>
+      <main>
+        <h1>Main</h1>
+        <Outlet />
+      </main>
+      <Router />
+    </>
   );
 }
 
---
+++ custom-forms/src/pages/Home.tsx
@@ -0,0 +1,5 @@
+function HomePage(): JSX.Element {
+  return <h1>Home</h1>;
+}
+
+export default HomePage;
