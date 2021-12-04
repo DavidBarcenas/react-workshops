3db9c9a24115490b964cac814b1a8dda1a70dfe0
David Barcenas <daveepro@outlook.com>
2021-12-03 18:29:45 -0600

    remove test projects

--- state-init-card/test/data/products.ts
+++
@@ -1,6 +0,0 @@
-import { Product } from "../../src/types/product";
-
-export const products: Product[] = [
-  { id: '1', title: ' Coffee Mug', image: './coffee-mug.png' },
-  { id: '2', title: ' Coffee Mug - Meme', image: './coffee-mug2.png' },
-];
