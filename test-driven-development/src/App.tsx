1a07a32a98541c672822341b9237d05f1791e33e
David Barcenas <daveepro@outlook.com>
2021-12-07 19:48:42 -0600

    add linters configuration

--- test-driven-development/src/App.tsx
+++ test-driven-development/src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
@@ -14,8 +13,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
