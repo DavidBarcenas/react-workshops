cb11bdf8009fcb19a9735135f5dbcb41cf053014
David Barcenas <daveepro@outlook.com>
2021-12-08 11:13:42 -0600

    validate that inputs form exists

--- test-driven-development/src/__tests__/pages/Form.test.tsx
+++ test-driven-development/src/__tests__/pages/Form.test.tsx
@@ -2,12 +2,21 @@ import { render, screen } from '@testing-library/react';
 import FormPage from '../../pages/Form';
 
 describe('When form is mounted', () => {
+  beforeEach(() => render(<FormPage />));
+
   it('There must be a create product form page.', () => {
-    render(<FormPage />);
-    expect(
-      screen.getByRole('heading', {
-        name: /create product/i,
-      })
-    ).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: /create product/i })).toBeInTheDocument();
+  });
+
+  it('The following fields must exist: name. size, type', () => {
+    expect(screen.getByLabelText(/name/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/size/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/type/i)).toBeInTheDocument();
+  });
+
+  it('The type field should have the following options (electronic, furniture, clothing)', () => {
+    expect(screen.queryByText(/electronic/i)).toBeInTheDocument();
+    expect(screen.queryByText(/furniture/i)).toBeInTheDocument();
+    expect(screen.queryByText(/clothing/i)).toBeInTheDocument();
   });
 });
--- test-driven-development/src/pages/Form.tsx
+++ test-driven-development/src/pages/Form.tsx
@@ -1,5 +1,19 @@
 function FormPage(): JSX.Element {
-  return <h1>Create Product</h1>;
+  return (
+    <>
+      <h1>Create Product</h1>
+      <label htmlFor="name">Name</label>
+      <input type="text" name="name" id="name" />
+      <label htmlFor="size">Size</label>
+      <input type="text" name="size" id="size" />
+      <label htmlFor="type">Type</label>
+      <select name="type" id="type">
+        <option value="electronic">Electronic</option>
+        <option value="furniture">Furniture</option>
+        <option value="clothing">Clothing</option>
+      </select>
+    </>
+  );
 }
 
 export default FormPage;
