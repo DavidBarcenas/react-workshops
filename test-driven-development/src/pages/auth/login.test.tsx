2e2479a8c094dbfd5eff6fb89f3efc7bc8e87b56
David Barcenas <daveepro@outlook.com>
2021-12-20 17:36:58 -0600

    validate that there are email, password and submit button fields

--- test-driven-development/src/pages/auth/login.test.tsx
+++ test-driven-development/src/pages/auth/login.test.tsx
@@ -1,9 +1,16 @@
-import {render, screen} from "@testing-library/react";
-import LoginPage from "./login";
+import { render, screen } from '@testing-library/react';
+import LoginPage from './login';
+
+beforeEach(() => render(<LoginPage />));
 
 describe('login page is mounted', () => {
   it('must display the login title', () => {
-    render(<LoginPage />)
-    expect(screen.getByText(/login page/i)).toBeInTheDocument()
-  })
-})
+    expect(screen.getByText(/login page/i)).toBeInTheDocument();
+  });
+
+  it('must have a form with the following fields: email, password and a submit button', () => {
+    expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /send/i })).toBeInTheDocument();
+  });
+});
--- test-driven-development/src/pages/auth/login.tsx
+++ test-driven-development/src/pages/auth/login.tsx
@@ -1,3 +1,13 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 export default function LoginPage() {
-  return <h1>Login Page</h1>
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <TextField label="email" id="email" />
+      <TextField label="password" id="password" type="password" />
+      <Button>Send</Button>
+    </div>
+  );
 }
