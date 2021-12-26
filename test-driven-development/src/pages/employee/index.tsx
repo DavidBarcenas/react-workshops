2c2f007a451967a2beb95ab638f86ca501483f9f
David Barcenas <daveepro@outlook.com>
2021-12-26 17:32:02 -0600

    add navbar to admin page

--- test-driven-development/src/pages/employee/index.tsx
+++ test-driven-development/src/pages/employee/index.tsx
@@ -1,3 +1,10 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
 import { useContext } from 'react';
 import { ADMIN_ROLE } from '../../consts/messages';
 import { AuthContext } from '../../state/auth-context';
@@ -5,10 +12,29 @@ import { AuthContext } from '../../state/auth-context';
 export default function EmployeePage() {
   const { user } = useContext(AuthContext);
   return (
-    <>
-      <h1>Employee Page</h1>
-      {user.role === ADMIN_ROLE && <button>Delete</button>}
-      <p>{user.username}</p>
-    </>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>
+            Employee Page
+          </Typography>
+          {user.role === ADMIN_ROLE && (
+            <Button type="button" color="inherit">
+              Delete
+            </Button>
+          )}
+          <Button color="inherit">{user.username}</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
