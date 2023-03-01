bdbf1035525934e6749a8fd3baebbb497a9a4952
David Barcenas <dbarcenas.dev@gmail.com>
2023-02-28 22:53:24 -0600

    refresh-react: create app layout

--- refresh-react-18/src/App.tsx
+++ refresh-react-18/src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./app.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <article className="tw-follow-card">
+      <header className="tw-follow-header">
+        <img
+          className="tw-follow-avatar"
+          src="https://unavatar.io/sindresorhus@gmail.com"
+          alt="Avatar"
+        />
+        <div className="tw-follow-info">
+          <strong>David Bárcenas</strong>
+          <span className="tw-follow-username">@daveeb</span>
+        </div>
+      </header>
+      <aside>
+        <button className="tw-follow-btn">Seguir</button>
+      </aside>
+    </article>
+  );
 }
-
-export default App
