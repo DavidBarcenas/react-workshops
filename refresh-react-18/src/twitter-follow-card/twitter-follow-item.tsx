d0efd96e24e7515e28b003ab3dc1292a8a47134f
David Barcenas <dbarcenas.dev@gmail.com>
2023-03-03 12:09:48 -0600

    twitter-card: add followItem component

--- refresh-react-18/src/twitter-follow-card/twitter-follow-item.tsx
+++ refresh-react-18/src/twitter-follow-card/twitter-follow-item.tsx
@@ -15,33 +15,27 @@ export function TwitterFollowItem({ userName, name, initialIsFollowing = false }
   };
 
   return (
-    <div className='wrapper'>
-      <div className='fluid'>
-        <div className='fluid-content'>
-          <div className='env'>
-            <figure className='tw-follow-avatar'>
-              <img className='tw-follow-img' src='https://unavatar.io/daveepro' alt={name} />
-            </figure>
-            <header className='tw-follow-header'>
-              <strong>David Barcenas con mucho texto</strong>
-              <span className='tw-follow-username'>@daveepro</span>
-            </header>
+    <div className='tw-follow-item'>
+      <header className='tw-follow-header'>
+        <div className='tw-follow-wrap'>
+          <img className='tw-follow-avatar' src='https://unavatar.io/daveepro' alt={name} />
+          <div className='tw-follow-info'>
+            <strong>David Barcenas con mucho texto</strong>
+            <span className='tw-follow-username'>@daveepro</span>
           </div>
         </div>
-      </div>
-      <div className='static'>
-        <div className='static-content'>
-          <aside className='tw-follow-aside'>
-            <button
-              onClick={handleFollowing}
-              className={`tw-follow-btn ${isFollowing ? 'is-following' : ''}`}
-            >
-              <span className='tw-follow-text'>{isFollowing ? 'Siguiendo' : 'Seguir'}</span>
-              <span className='tw-stop-following'>Dejar de seguir</span>
-            </button>
-          </aside>
+      </header>
+      <aside className='tw-follow-aside'>
+        <div className='tw-follow-btn-wrap'>
+          <button
+            onClick={handleFollowing}
+            className={`tw-follow-btn ${isFollowing ? 'is-following' : ''}`}
+          >
+            <span className='tw-follow-text'>{isFollowing ? 'Siguiendo' : 'Seguir'}</span>
+            <span className='tw-stop-following'>Dejar de seguir</span>
+          </button>
         </div>
-      </div>
+      </aside>
     </div>
   );
 }
