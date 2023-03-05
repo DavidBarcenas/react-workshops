bc1ff5a974c0512e09364457320da5b8fa10bd1a
David Barcenas <dbarcenas.dev@gmail.com>
2023-03-04 21:16:19 -0600

    todo: add footer

---
+++ refresh-react-18/src/todo-app/footer.tsx
@@ -0,0 +1,31 @@
+import { Filters } from './filters';
+import { type TodoFilters } from './types';
+
+interface Props {
+  activeCount: number;
+  completedCount: number;
+  filterSelected: (typeof TodoFilters)[keyof typeof TodoFilters];
+  handleFilterChange: (filter: (typeof TodoFilters)[keyof typeof TodoFilters]) => void;
+  onClearCompleted: () => void;
+}
+
+export function Footer({
+  activeCount = 0,
+  completedCount = 0,
+  filterSelected,
+  handleFilterChange,
+  onClearCompleted,
+}: Props) {
+  return (
+    <footer>
+      <span>
+        <strong>{activeCount}</strong>
+        tareas pendiendtes
+      </span>
+
+      <Filters filterSelected={filterSelected} onFilterChange={handleFilterChange} />
+
+      {completedCount > 0 && <button onClick={onClearCompleted}>Limpiar completados</button>}
+    </footer>
+  );
+}
