b9eb76e3f73b2426b0767782fda573244985ff26
David Barcenas <dbarcenas.dev@gmail.com>
2023-04-19 22:19:18 -0600

    add reset game to winner modal

--- refresh-react-18/src/tic-tac-toe/components/winner-modal.tsx
+++ refresh-react-18/src/tic-tac-toe/components/winner-modal.tsx
@@ -1,14 +1,18 @@
+import { ResetButton } from "./reset-button";
+
 interface Props {
   winner: any;
+  resetGame: () => void;
 }
 
-export function WinnerModal({ winner }: Props) {
+export function WinnerModal({ winner, resetGame }: Props) {
   return winner !== null ? (
     <section className='fixed top-0 left-0 w-full h-full bg-black/70 grid place-content-center animate-zoom'>
       <div className='bg-orange-300 min-w-[600px] py-10'>
         <h2 className='text-5xl font-bold mb-4'>
-          {winner === false ? 'Empate' : 'Ganó: ' + winner}
+          {!winner ? 'Empate' : 'Ganó: ' + winner}
         </h2>
+        <ResetButton resetGame={resetGame} />
       </div>
     </section>
   ) : null;
