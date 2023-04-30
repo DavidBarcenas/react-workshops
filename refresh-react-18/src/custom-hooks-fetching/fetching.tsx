47048ee61d916cfd0e6f721f9794fea495dc0371
David Barcenas <dbarcenas.dev@gmail.com>
2023-04-29 20:08:48 -0600

    implement custom hooks

--- refresh-react-18/src/custom-hooks-fetching/fetching.tsx
+++ refresh-react-18/src/custom-hooks-fetching/fetching.tsx
@@ -1,40 +1,20 @@
-import { useEffect, useState } from 'react';
-import { CatImage } from './models/ResponseAPI';
-import { getRandomFact } from './services/facts';
-
-const CAT_PICTURE_API = `https://cataas.com`;
+import { useCatPicture } from './hooks/cat-picture';
+import { useCatFact } from './hooks/cat-fact';
 
 function Fetching() {
-  const [fact, setFact] = useState<undefined | string>();
-  const [catPicture, setCatPicture] = useState<undefined | string>();
-
-  const getCatFact = () => {
-    getRandomFact().then(setFact);
-  };
-
-  useEffect(getCatFact, []);
-
-  useEffect(() => {
-    if (!fact) return;
-
-    const threeFirstWords = fact.split(' ', 3).join(' ');
-    fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=300&json=true`)
-      .then((res) => res.json())
-      .then((data: CatImage) => {
-        setCatPicture(data.url);
-      });
-  }, [fact]);
+  const { fact, refreshFact } = useCatFact();
+  const { catPicture } = useCatPicture(fact);
 
   return (
     <main>
       {fact && <p>{fact}</p>}
       {catPicture && (
         <img
-          src={`${CAT_PICTURE_API}/${catPicture}`}
+          src={catPicture}
           alt={`Image extracted using three first words of the fact: ${fact}`}
         />
       )}
-      <button onClick={getCatFact}>Get cat fact</button>
+      <button onClick={refreshFact}>Get cat fact</button>
     </main>
   );
 }
