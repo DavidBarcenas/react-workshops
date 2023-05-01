f9d79fa6dfda195398aeca2b6bfce501479649a6
David Barcenas <dbarcenas.dev@gmail.com>
2023-04-30 21:09:44 -0600

    add custom grid

--- refresh-react-18/src/movie-search/components/movies.tsx
+++ refresh-react-18/src/movie-search/components/movies.tsx
@@ -6,13 +6,13 @@ interface Props {
 
 export function Movies({ list }: Props) {
   return list.length ? (
-    <ul className='grid grid-cols-5 gap-8 m-0 p-0'>
+    <ul className='grid grid-cols-auto-fill gap-8 m-0 p-0'>
       {list.map((movie) => (
         <li key={movie.id}>
-          <img src={movie.poster} alt={movie.title} className='w-full max-h-[80%] object-contain' />
-          <div className='p-4'>
-            <h2 className='text-xl font-bold'>{movie.title}</h2>
-            <div className='flex items-center justify-between'>
+          <img src={movie.poster} alt={movie.title} className='w-full min-h-[300px] object-cover' />
+          <div className='p-2'>
+            <h2 className='font-bold mb-2'>{movie.title}</h2>
+            <div className='flex items-center justify-between text-sm text-gray-400'>
               <span>{movie.year}</span>
               <span>{movie.type}</span>
             </div>
--- refresh-react-18/tailwind.config.cjs
+++ refresh-react-18/tailwind.config.cjs
@@ -1,23 +1,26 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       animation: {
-        zoom: 'zoomIn .2s ease-in-out',
+        zoom: "zoomIn .2s ease-in-out"
       },
       backgroundImage: {
-        'todo-app': 'linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)',
-        'tic-tac-toe':
-          'radial-gradient( circle farthest-corner at 39.2% 57.3%,  rgba(20,82,111,1) 30.9%, rgba(25,67,86,1) 74.6% )',
+        "todo-app": "linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)",
+        "tic-tac-toe":
+          "radial-gradient( circle farthest-corner at 39.2% 57.3%,  rgba(20,82,111,1) 30.9%, rgba(25,67,86,1) 74.6% )"
+      },
+      gridTemplateColumns: {
+        "auto-fill": "repeat(auto-fill, minmax(200px, 1fr))"
       },
       keyframes: {
         zoomIn: {
-          '0%': { transform: 'scale(0)' },
-          '100%': { transform: 'scale(1)' },
-        },
-      },
-    },
+          "0%": { transform: "scale(0)" },
+          "100%": { transform: "scale(1)" }
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
