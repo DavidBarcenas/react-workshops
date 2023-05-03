d39ed309a8c6ad71fd9e2aa4d062a7e1fd275839
David Barcenas <dbarcenas.dev@gmail.com>
2023-05-02 21:50:34 -0600

    update movie list styles

--- refresh-react-18/src/movie-search/components/movies.tsx
+++ refresh-react-18/src/movie-search/components/movies.tsx
@@ -10,7 +10,7 @@ export function Movies({ list }: Props) {
       {list.map((movie) => (
         <li key={movie.id}>
           <img src={movie.poster} alt={movie.title} className='w-full min-h-[300px] object-cover' />
-          <div className='p-2'>
+          <div className='py-2'>
             <h2 className='font-bold mb-2'>{movie.title}</h2>
             <div className='flex items-center justify-between text-sm text-gray-400'>
               <span>{movie.year}</span>
